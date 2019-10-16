51d1053725f8fab39d494df0d5b2aa8499ff835d
Wellington Viana <wellvsilva@gmail.com>
2019-10-16 17:29:02 -0300

    adicionando o happynumbers

---
+++ happynumbers.py
@@ -0,0 +1,17 @@
+def happy(number):
+    if number in (1, 10, 100):
+        string = str(number)
+
+        total = 0
+        for char in string:
+            total += int(char)
+
+        return total == 1
+
+    return False
+
+
+assert happy(1)
+assert happy(10)
+assert happy(100)
+assert not happy(4)
