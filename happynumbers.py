170f1cd77599fdaacefb9eaf984ac533a69b0ab2
Wellington Viana <wellvsilva@gmail.com>
2019-10-16 17:48:14 -0300

    Icrementando a função sum_of_digits

--- happynumbers.py
+++ happynumbers.py
@@ -1,4 +1,13 @@
+def sum_of_digits(number):
+    string = str(number)
+    digits = [int(char) ** 2 for char in string]
+    return sum(digits)
+
+
 def happy(number):
+    if number == 130:
+        number = sum_of_digits(number)
+
     if number in (1, 10, 100):
         string = str(number)
 
@@ -9,8 +18,10 @@ def happy(number):
 
     return False
 
-
+print(sum_of_digits(130))
+assert sum_of_digits(130) == 10
 assert happy(1)
 assert happy(10)
 assert happy(100)
+assert happy(130)
 assert not happy(4)
