ce896096dbcc1f417cbc2f9469e5e5b3ceadfd65
Wellington Viana <wellvsilva@gmail.com>
2019-10-17 16:07:20 -0300

    Versão final do happy number

--- happynumbers.py
+++ happynumbers.py
@@ -1,20 +1,16 @@
-def sum_of_squares(number):
-    string = str(number)
-    digits = [int(char) ** 2 for char in string]
-    return sum(digits)
-
-
+"""
+Um número feliz é definido pelo seguinte processo:
+Começando com qualquer número inteiro positivo, substitua o número pela soma dos
+quadrados de seus dígitos e repita o processo até que o número seja igual a   1
+(onde permanecerá) ou faça um loop infinito em um ciclo que não inclui   1 .
+Os números para os quais esse processo termina em   1   são números felizes,
+enquanto os que não terminam em   1   são números infelizes.
+Exiba um exemplo de sua saída aqui.
+"""
 def happy(number):
-    box = []
-    n = number
-    while n != 1 and n not in box:
-        box.append(n)
-        n = sum_of_squares(n)
-    return n == 1
-
-    return False
+    next_ = sum(int(char) ** 2 for char in str(number))
+    return number in (1, 7) if number < 10 else happy(next_)
 
 
-assert sum_of_squares(130) == 10
 assert all([happy(n) for n in (1, 10, 100, 130, 97)])
 assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
